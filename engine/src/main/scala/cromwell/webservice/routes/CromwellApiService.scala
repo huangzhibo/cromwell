f8575cf1b62804b603e2552cd2b52e9d93e84051
geoffjentry <geoffjentry@gmail.com>
2018-11-06 12:49:14 -0500

    checkpoint

--- engine/src/main/scala/cromwell/webservice/routes/CromwellApiService.scala
+++ engine/src/main/scala/cromwell/webservice/routes/CromwellApiService.scala
@@ -8,7 +8,7 @@ import akka.http.scaladsl.marshalling.{ToEntityMarshaller, ToResponseMarshallabl
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
 import akka.pattern.{AskTimeoutException, ask}
 import akka.stream.ActorMaterializer
 import akka.util.{ByteString, Timeout}
@@ -111,31 +111,7 @@ trait CromwellApiService extends HttpInstrumentation
     path("workflows" / Segment / Segment / "abort") { (_, possibleWorkflowId) =>
       post {
         instrumentRequest {
-          def sendAbort(workflowId: WorkflowId): Route = {
-            val response = workflowStoreActor.ask(WorkflowStoreActor.AbortWorkflowCommand(workflowId)).mapTo[AbortResponse]
-
-            onComplete(response) {
-              case Success(WorkflowAbortingResponse(id, restarted)) =>
-                workflowManagerActor ! AbortWorkflowCommand(id, restarted)
-                complete(ToResponseMarshallable(WorkflowAbortResponse(id.toString, WorkflowAborting.toString)))
-              case Success(WorkflowAbortFailureResponse(_, e: IllegalStateException)) =>
-                /*
-                  Note that this is currently impossible to reach but was left as-is during the transition to akka http.
-                  When aborts get fixed, this should be looked at.
-                */
-                e.errorRequest(StatusCodes.Forbidden)
-              case Success(WorkflowAbortFailureResponse(_, e: WorkflowNotFoundException)) => e.errorRequest(StatusCodes.NotFound)
-              case Success(WorkflowAbortFailureResponse(_, e)) => e.errorRequest(StatusCodes.InternalServerError)
-              case Failure(_: AskTimeoutException) if CromwellShutdown.shutdownInProgress() => serviceShuttingDownResponse
-              case Failure(e: TimeoutException) => e.failRequest(StatusCodes.ServiceUnavailable)
-              case Failure(e) => e.errorRequest(StatusCodes.InternalServerError)
-            }
-          }
-
-          Try(WorkflowId.fromString(possibleWorkflowId)) match {
-            case Success(workflowId) => sendAbort(workflowId)
-            case Failure(_) => InvalidWorkflowException(possibleWorkflowId).failRequest(StatusCodes.BadRequest)
-          }
+          abortWorkflow(possibleWorkflowId, workflowStoreActor, workflowManagerActor)
         }
       }
     } ~
@@ -242,6 +218,43 @@ object CromwellApiService {
     def errorRequest(statusCode: StatusCode, warnings: Seq[String] = Vector.empty): Route = {
       completeResponse(statusCode, APIResponse.error(e).toJson.prettyPrint, warnings)
     }
+  }
+
+  def abortWorkflow(possibleWorkflowId: String,
+                    workflowStoreActor: ActorRef,
+                    workflowManagerActor: ActorRef,
+                    errorHandler: PartialFunction[Throwable, Route] = standardAbortErrorHandler)(implicit timeout: Timeout): Route = {
+    def sendAbort(workflowId: WorkflowId): Route = {
+      val response = workflowStoreActor.ask(WorkflowStoreActor.AbortWorkflowCommand(workflowId)).mapTo[AbortResponse]
+
+      onComplete(response) {
+        case Success(WorkflowAbortingResponse(id, restarted)) =>
+          workflowManagerActor ! AbortWorkflowCommand(id, restarted)
+          complete(ToResponseMarshallable(WorkflowAbortResponse(id.toString, WorkflowAborting.toString)))
+        /*
+          Note that this is currently impossible to reach but was left as-is during the transition to akka http.
+          When aborts get fixed, this should be looked at.
+        */
+        case Success(WorkflowAbortFailureResponse(_, e: IllegalStateException)) => throw e
+        case Success(WorkflowAbortFailureResponse(_, e)) => throw e
+        case Failure(e) => throw e
+      }
+    }
+
+    handleExceptions(ExceptionHandler(errorHandler)) {
+      Try(WorkflowId.fromString(possibleWorkflowId)) match {
+        case Success(workflowId) => sendAbort(workflowId)
+        case Failure(_) => InvalidWorkflowException(possibleWorkflowId).failRequest(StatusCodes.BadRequest)
+      }
+    }
+  }
+
+  private def standardAbortErrorHandler: PartialFunction[Throwable, Route] = {
+    case e: IllegalStateException => e.errorRequest(StatusCodes.Forbidden)
+    case e: WorkflowNotFoundException => e.errorRequest(StatusCodes.NotFound)
+    case _: AskTimeoutException if CromwellShutdown.shutdownInProgress() => serviceShuttingDownResponse
+    case e: TimeoutException => e.failRequest(StatusCodes.ServiceUnavailable)
+    case e: Exception => e.errorRequest(StatusCodes.InternalServerError)
   }
 
   def validateWorkflowId(possibleWorkflowId: String,
--- engine/src/main/scala/cromwell/webservice/routes/wes/WesRouteSupport.scala
+++ engine/src/main/scala/cromwell/webservice/routes/wes/WesRouteSupport.scala
@@ -25,27 +25,37 @@ trait WesRouteSupport extends HttpInstrumentation {
 
   /*
     These routes are not currently going through the MetadataBuilderRegulator/MetadataBuilder. This is for two reasons:
-        - It'd require a fairly substantial refactor of the MetadataBuilderActor to be more general
-        - It's expected that for now the usage of these endpoints will not be extensive, so the protections of the regulator
-           should not be necessary
+      - It'd require a fairly substantial refactor of the MetadataBuilderActor to be more general
+      - It's expected that for now the usage of these endpoints will not be extensive, so the protections of the regulator
+         should not be necessary
     */
-  val wesRoutes: Route = concat(
-    pathPrefix("ga4gh" / "wes" / "v1" / "runs") {
-      path(Segment / "status") { possibleWorkflowId =>
-
-        val response  = validateWorkflowId(possibleWorkflowId, serviceRegistryActor).flatMap(w => serviceRegistryActor.ask(GetStatus(w)).mapTo[MetadataServiceResponse])
-        // WES can also return a 401 or a 403 but that requires user auth knowledge which Cromwell doesn't currently have
-        onComplete(response) {
-          case Success(s: StatusLookupResponse) =>
-            val wesState = WesState.fromCromwellStatus(s.status)
-            complete(WesRunStatus(s.workflowId.toString, wesState))
-          case Success(r: FailedMetadataResponse) => r.reason.errorRequest(StatusCodes.InternalServerError)
-          case Failure(e: UnrecognizedWorkflowException) => complete(NotFoundError)
-          case Failure(e) => complete(WesErrorResponse(e.getMessage, StatusCodes.InternalServerError.intValue))
+  val wesRoutes: Route =
+    instrumentRequest {
+      concat(
+        pathPrefix("ga4gh" / "wes" / "v1" / "runs") {
+          concat(
+            path(Segment / "status") { possibleWorkflowId =>
+              val response  = validateWorkflowId(possibleWorkflowId, serviceRegistryActor).flatMap(w => serviceRegistryActor.ask(GetStatus(w)).mapTo[MetadataServiceResponse])
+              // WES can also return a 401 or a 403 but that requires user auth knowledge which Cromwell doesn't currently have
+              onComplete(response) {
+                case Success(s: StatusLookupResponse) =>
+                  val wesState = WesState.fromCromwellStatus(s.status)
+                  complete(WesRunStatus(s.workflowId.toString, wesState))
+                  // FIXME: waht about MetadataServiceResponses *not* StatusLookupReponse ?
+                  // FIXME the below is almost certainly not right (it's from the metadata builder actor)
+                case Success(r: FailedMetadataResponse) => r.reason.errorRequest(StatusCodes.InternalServerError)
+                case Failure(_: UnrecognizedWorkflowException) => complete(NotFoundError)
+                case Failure(e) => complete(WesErrorResponse(e.getMessage, StatusCodes.InternalServerError.intValue))
+              }
+            }
+//            ,
+//            path(Segment / "cancel") { possibleWorkflowId =>
+//
+//            }
+          )
         }
-      }
+      )
     }
-  )
 }
 
 object WesRouteSupport {
